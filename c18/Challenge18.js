b00269d8e973a4eba93ed53fed7524b9fa332afa
TaufikAlex <Taufik_Alex@Yahoo.com>
2019-08-22 17:06:04 +0700

    Challenge 18

---
+++ c18/Challenge18.js
@@ -0,0 +1,6 @@
+const fs =require("fs");
+
+console.log("Welcome to Universitas Pendidikan Indonesia")
+console.log("Jl setiabudi No. 255");
+console.log("=======================================================")
+
